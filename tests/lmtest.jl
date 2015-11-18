e9560db76ad004d8879759761436202bb1959c3e
Saunak Sen <sen@biostat.ucsf.edu>
2015-11-18 12:00:36 -0600

    Test of Karl-like scenario.

--- tests/lmtest.jl
+++ tests/lmtest.jl
@@ -1,6 +1,21 @@
 # include the function
 include("../code/lm.jl")
 
+N = 1000
+P = 1
+
+o = ones(Float64,(N,1));
+x = randn( RandomDevice(), (N,P) ) * 10 + 50 ;
+e = randn( RandomDevice(), (N,1) ) *2.5 ;
+b = [30, 0.5;]
+X = [o x];
+y = X*b + e;
+
+@time lm0(y,X);
+@time lm1(y,X);
+@time lm2(y,X);
+@time lm3(y,X);
+
 
 # testing the lm function
 
