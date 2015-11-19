9607db1fc4d3d1f695bc99e4632b951c8d3e1d53
Saunak Sen <sen@biostat.ucsf.edu>
2015-11-18 18:07:07 -0600

    Tests similar to Karl augmented to larger mats.

--- tests/lmtest.jl
+++ tests/lmtest.jl
@@ -17,19 +17,19 @@ y = X*b + e;
 @time lm3(y,X);
 
 
-# testing the lm function
+N = 2500
+P = 300
 
-N = 10000
-P = 1000
-
-X = randn( RandomDevice(), (N,P) );
-e = randn( RandomDevice(), (N,1) );
-b = [(1:P);]
-
+o = ones(Float64,(N,1));
+x = randn( RandomDevice(), (N,P-1) ) * 10 + 50 ;
+e = randn( RandomDevice(), (N,1) ) *2.5 ;
+b = [randn( RandomDevice(), (P,1) );]
+X = [o x];
 y = X*b + e;
 
 @time lm0(y,X);
 @time lm1(y,X);
 @time lm2(y,X);
 @time lm3(y,X);
+
 
