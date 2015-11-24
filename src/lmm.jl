ae901253bfc4fdb6c0f57a1d9ffc012e214f93e1
Saunak Sen <sen@uthsc.edu>
2015-11-24 17:13:07 -0600

    Working optimization routine; needs to be finished.

--- src/lmm.jl
+++ src/lmm.jl
@@ -83,38 +83,60 @@ function wls(y::Array{Float64,2},X::Array{Float64,2},w::Array{Float64,1})
 
 end
 
+function invlogit(x::Float64)
+    exp(x)/(1+exp(x))
+end
+    
 # ################################################################
 # function to calculate log likelihood of data given fixed effects
 # ################################################################
 """
 logLik: log likelihood of data
 """
-function logLik(sigma2,h2,y,X,d)
+function logLik(logsigma2::Float64,logith2::Float64,
+                y::Array{Float64,2},
+                X::Array{Float64,2},
+                d::Array{Float64,1})
     # weights
-    w =sigma2( h2*d + (1-h2) )
+    w =exp(logsigma2) * ( invlogit(logith2)*d + (1-invlogit(logith2)) )
+
+    (b,rss) = wls(y,X,w)
+    yhat = X*b
     sqrtw = sqrt(w)
+
+    n = size(w,1)
     # get normal pdfs
-    lp =logpdf(Normal(),(y-hyat)/sqrtw)-log(sqrtw)
+    lp = -rss/2 - 0.5 * sum(log(w)) 
     # sum to get log likelihood
-    return sum(lp)
+    return lp[1,1]
 end
 
-# ################################################################
-# function to estimate heritability given fixed effects
-# ################################################################
+##################################################################
+# function to estimate variance components and heritability
+##################################################################
 
 """
 estVarComp: estimate variance components
 """
 
-function estVarComp(y,X,d)
 
-    function logLik0(sigma2,h2)
-        logLik(sigma2,h2,y,X,d)
+function estVarComp(y::Array{Float64,2},
+                    X::Array{Float64,2},
+                    d::Array{Float64,1},logsigma2::Float64,logith2::Float64)
+
+    function logLik0(z::Array{Float64,1})
+        -logLik(z[1],z[2],y,X,d)
     end
 
-    optimize(logLik0,[1, 0.01],method = :l_bfgs)
+    est = optimize(logLik0,[logsigma2,logith2])
+    return exp(est.minimum[1]) , invlogit(est.minimum[2])
 end
 
+##################################################################
+# function to fit mixed model
+##################################################################
 
+function lmm( y::Array{Float64,2}, X::Array{Float64,2},
+                    d::Array{Float64,1},logsigma2::Float64,logith2::Float64)
+                    )
 
