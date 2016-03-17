b3928476f7f959ed0d4b563a761444344a876355
Saunak Sen <sen@uthsc.edu>
2016-03-17 16:48:30 -0500

    Correction for REML log likelihood.

--- src/lmm.jl
+++ src/lmm.jl
@@ -83,7 +83,7 @@ function wls(y::Array{Float64,2},X::Array{Float64,2},w::Array{Float64,1},
     if(any(w.<=.0))
         error("Some weights are not positive.")
     end
-        
+         
     # square root of the weights
     sqrtw = sqrt(w)
     # scale by weights
@@ -107,10 +107,10 @@ function wls(y::Array{Float64,2},X::Array{Float64,2},w::Array{Float64,1},
     end
 
     # return coefficient and variance estimate
-    logdetSigma = n*log(sigma2) - sum(log(w))
-    ell = -0.5 * ( logdetSigma + rss/sigma2 )
+    logdetSigma = n*log(sigma2) - sum(log(w)) 
+    ell = -0.5 * ( logdetSigma + rss/sigma2 ) 
     if ( reml )
-        ell -=  log(abs(det(r)))
+        ell -=  log(abs(det(r))) - (p/2)*(log(sigma2))
     end
 
     return Wls(b,sigma2,ell)
@@ -136,7 +136,7 @@ function lmm(y::Array{Float64,2},
              X::Array{Float64,2},
              lambda::Array{Float64,1},
              reml::Bool=false)
-
+    
     function logLik0(h2::Float64)
         - wls(y,X,1./(h2*lambda+(1-h2)),reml,true).ell
     end
