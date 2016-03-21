d5aa9da696a59098e78c8bc639177a49a255cb74
Saunak Sen <sen@uthsc.edu>
2016-03-21 18:07:18 -0500

    Code for capturing results and runtimes in R.

---
+++ tests/lmmtest-lmmlite.R
@@ -0,0 +1,64 @@
+library(lmmlite)
+data(recla)
+n_phe <- ncol(recla$pheno)
+lmmlite <- data.frame(index=rep(NA,2*n_phe),
+                      method=rep("", 2*n_phe),
+                      hsq=rep(NA,2*n_phe),
+                      intercept=rep(NA,2*n_phe),
+                      sex=rep(NA,2*n_phe),
+                      sigmasq=rep(NA,2*n_phe),
+                      loglik=rep(NA,2*n_phe), stringsAsFactors=FALSE)
+
+all_e <- vector("list", n_phe)
+for(i in 1:n_phe) {
+    keep <- !is.na(recla$pheno[,i])
+    y <- recla$pheno[keep,i,drop=FALSE]
+    x <- recla$covar[keep,]
+    k <- recla$kinship[keep,keep]
+
+    all_e[[i]] <- e <- eigen_rotation(k, y, x)
+    res <- fitLMM(e$Kva, e$y, e$X, reml=TRUE)
+    lmmlite[i*2-1,1] <- i
+    lmmlite[i*2-1,2] <- "reml"
+    lmmlite[i*2-1,-(1:2)] <- c(res$hsq, res$beta, res$sigmasq, res$loglik)
+
+    res <- fitLMM(e$Kva, e$y, e$X, reml=FALSE)
+    lmmlite[i*2,1] <- i
+    lmmlite[i*2,2] <- "ml"
+    lmmlite[i*2,-(1:2)] <- c(res$hsq, res$beta, res$sigmasq, res$loglik)
+}
+
+##################################################################
+## comparison of results
+##################################################################
+
+juliaRes <- read.csv("results.csv")
+lmmlite$hsq[lmmlite$method=="reml"] - juliaRes$h2   
+log(lmmlite$sigmasq[lmmlite$method=="reml"]/juliaRes$sigma2)
+
+#################################################################
+## performance comparison
+#################################################################
+
+library(microbenchmark)
+library(broman)
+
+time_full2 <- microbenchmark(R={
+    for(i in 1:ncol(recla$pheno)) {
+        keep <- !is.na(recla$pheno[,i])
+        eigenrot <- eigen_rotation(recla$kinship[keep,keep],
+                                   recla$pheno[keep,i],
+                                   recla$covar[keep,], use_cpp=FALSE)
+        res <- fitLMM(eigenrot$Kva, eigenrot$y, eigenrot$X, reml=TRUE,
+                      use_cpp=FALSE)
+    }},
+    cpp={
+        for(i in 1:ncol(recla$pheno)) {
+            keep <- !is.na(recla$pheno[,i])
+            eigenrot <- eigen_rotation(recla$kinship[keep,keep],
+                                       recla$pheno[keep,i],
+                                       recla$covar[keep,], use_cpp=TRUE)
+            res <- fitLMM(eigenrot$Kva, eigenrot$y, eigenrot$X, reml=TRUE,
+                          use_cpp=TRUE)
+        }}, times=10)
+print(time_full2, digits=4)
