897bcf5f31b012e3a2b3e39b358f24440aded5e5
Saunak Sen <sen@uthsc.edu>
2019-04-02 15:50:12 -0500

    Modify wt function.; return heritability from scan.

--- src/scan.jl
+++ src/scan.jl
@@ -42,7 +42,7 @@ function scan(y::Array{Float64,2},g::Array{Float64,2},
         lod[i] = (n/2)*(log10(out0[1]) - log10(out1[1]))
     end
 
-    return lod
+    return ( out00.sigma2, out00.h2, lod )
 
 end
 
@@ -62,7 +62,7 @@ function scan(y::Array{Float64,2},g::Array{Float64,2},
     # fit null lmm
     out00 = flmm(y0,reshape(X0[:,1], :, 1),lambda0,reml)
     # weights proportional to the variances
-    wts = makeweights( out00.sigma2,out00.h2,lambda0 )
+    wts = makeweights( out00.h2,lambda0 )
     # rescale by weights
     rowScale!(y0,sqrt.(wts))
     rowScale!(X0,sqrt.(wts))
@@ -92,7 +92,7 @@ function scan(y::Array{Float64,2},g::Array{Float64,2},
 
 end
 
-function makeweights( sigma2::Float64, h2::Float64,
+function makeweights( h2::Float64,
                       lambda::Array{Float64,1} )
     return h2*lambda .+ (1.0-h2)
 end
