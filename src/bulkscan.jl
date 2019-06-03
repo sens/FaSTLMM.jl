ff80080866ddaa183a47c067c4e2be6e012bc873
Saunak Sen <sen@uthsc.edu>
2019-06-03 16:17:16 -0500

    Bulk estimation of heritability.

--- src/bulkscan.jl
+++ src/bulkscan.jl
@@ -7,9 +7,11 @@ using Distributed
 using Random
 using LinearAlgebra
 using SharedArrays
+using Optim
 
 include("scan.jl")
 include("util.jl")
+include("wls.jl")
 
 ################################################################
 # plain genome scan
@@ -17,9 +19,6 @@ include("util.jl")
 
 function bulkscan(y::Array{Float64,2},g::Array{Float64,2})
 
-    # number of markers
-    (n,m) = size(g)
-    
     y0 = deepcopy(y)
     g0 = deepcopy(g)
 
@@ -30,17 +29,70 @@ function bulkscan(y::Array{Float64,2},g::Array{Float64,2})
     return lod
 end
 
-# calculate lod scores
+
+# estimate heritability
+
+function esth2(y::Matrix{Float64},X::Matrix{Float64},
+               lambda::Vector{Float64})
+    est = flmm(y,X,lambda,false)
+    return est.h2
+end
+
+function esth2_grid(y::Matrix{Float64},X::Matrix{Float64},
+                    lambda::Vector{Float64},ngrid::Int64)
+    
+    h2grid = (0:(ngrid-1)+0.5)/(ngrid)
+    loglik = zeros(ngrid)
+
+    
+    for i=1:ngrid
+        wts = makeweights(h2grid[i],lambda)
+        # rowScale!(y0,sqrt.(wts))
+        # rowScale!(X0,sqrt.(wts))        
+        out = wls(y,X,1.0./wts,false,true)
+        loglik[i] = out.ell
+    end
+    return h2grid[argmax(loglik)]
+end
+
+
+# estimate heritability for multiple phenotypes
+
+function bulkesth2(y::Matrix{Float64},X::Matrix{Float64},
+                   lambda::Vector{Float64})
+    p = size(y,2)
+    h2 = zeros(p)
+    Threads.@threads for i=1:p
+    # for i=1:p    
+        h2[i] = esth2(reshape(y[:,i],:,1),X,lambda)
+    end
+    return h2
+end
+
+function bulkesth2_grid(y::Matrix{Float64},X::Matrix{Float64},
+                   lambda::Vector{Float64},ngrid::Int64)
+    p = size(y,2)
+    h2 = zeros(p)
+    Threads.@threads for i=1:p
+    # for i=1:p    
+        h2[i] = esth2_grid(reshape(y[:,i],:,1),X,lambda,ngrid)
+    end
+    return h2
+end
+
+# calculate LOD scores
 
 function calcLod(y::Matrix{Float64},g::Matrix{Float64})
 
     n = size(y,1)
     r = (y'*g) ./ n
-    # r = LinearAlgebra.BLAS.gemm('T','N',y,g) ./ n    
+    # r = LinearAlgebra.BLAS.gemm('T','N',y,g) ./ n
     r2lod!(r,n)
     return r
-    
+
 end
+
+# convert correlations to LOD scores
 
 function r2lod!(r::Matrix{Float64},n::Int64)
 
@@ -52,5 +104,20 @@ function r2lod!(r::Matrix{Float64},n::Int64)
             r[i,j] = c*log1p(-r[i,j]^2)
         end
     end
+    # return lod
+end
+
+function r2lod_dist!(r::Matrix{Float64},n::Int64)
+
+    c = -n/(2*log(10))
+    (nr,nc) = size(r)
+    r0 = SharedArray(r)
+    # lod = zeros(nr,nc)
+    @distributed for j=1:nc
+        for i=1:nr
+            r0[i,j] = c*log1p(-r0[i,j]^2)
+        end
+    end
+    r[:] = Matrix(r0)
     # return lod
 end
--- test/bulkscan-test.jl
+++ test/bulkscan-test.jl
@@ -1,6 +1,8 @@
 using BenchmarkTools
+using UnicodePlots
 
 include("../src/bulkscan.jl")
+include("../src/readData.jl")
 
 # number of individuals
 N = 100
@@ -14,3 +16,24 @@ y = randn(N,P);
 g = randn(N,M);
 
 @btime lod = bulkscan(y,g);
+
+pheno_file = "../data/bxdData/traits.csv"
+pheno = readBXDpheno(pheno_file)
+geno_file = "../data/bxdData/geno_prob.csv"
+geno = readGenoProb(geno_file)
+
+geno_output_file = "../data/bxdData/bxd_geno_for_gemma.txt"
+transform_bxd_geno_to_gemma(geno_file, geno_output_file);
+
+## run gemma:
+# Run this command in terminal to get kinship matrix from gemma. 
+run(`gemma -g $geno_output_file -p ../data/bxdData/pheno_for_gemma.txt -gk -no-check`)
+
+# getting kinship matrix from gemma 
+K = convert(Array{Float64,2},readdlm("./output/result.cXX.txt", '\t'));
+
+intercept = ones(size(pheno,1),1);
+(y0,X0,lambda) = rotateData(pheno,[intercept geno],K);
+
+h2 = bulkesth2(y0,reshape(X0[:,1],:,1),lambda);
+histogram(h2)
